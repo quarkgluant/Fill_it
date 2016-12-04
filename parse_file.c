6cddd3c248e774f39f11e157bd754a91f7d9318c
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2016-12-04 20:03:30 +0100

    2 nouvelles fonctions t_tet_list

--- parse_file.c
+++ parse_file.c
@@ -12,9 +12,9 @@
 
 #include "fill_it.h"
 
-t_list		*ft_create_elem(char data)
+t_list			*ft_create_elem(char data)
 {
-	t_list	*my_list;
+	t_list		*my_list;
 
 	my_list = malloc(sizeof(t_list));
 	if (my_list)
@@ -25,9 +25,28 @@ t_list		*ft_create_elem(char data)
 	return (my_list);
 }
 
-void		ft_list_push_back(t_list *begin_list, char data)
+t_tet_list		*create_tet(char *str)
 {
-	t_list	*tmp;
+	t_tet_list	*my_list;
+	int			i;
+
+	i = 0;
+	my_list = malloc(sizeof(t_tet_list));
+	if (my_list)
+	{
+		while (i < 16)
+		{
+			my_list->tetra[i] = str[i];
+			i++;		
+		}
+		my_list->next = NULL;
+	}
+	return (my_list);
+}
+
+void			ft_list_push_back(t_list *begin_list, char data)
+{
+	t_list		*tmp;
 
 	if (begin_list == NULL)
 	{
@@ -44,12 +63,31 @@ void		ft_list_push_back(t_list *begin_list, char data)
 	}
 }
 
-t_list		*parse_file(char *path)
+void			tet_list_push_back(t_tet_list *begin_list, char *str)
 {
-	t_list	*begin_list;
-	int		pos;
-	int		fd;
-	char	car;
+	t_tet_list	*tmp;
+
+	if (begin_list == NULL)
+	{
+		begin_list = create_tet(str);
+		if (begin_list == NULL)
+			exit(-1);
+	}
+	else
+	{
+		tmp = begin_list;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = create_tet(str);
+	}
+}
+
+t_list			*parse_file(char *path)
+{
+	t_list		*begin_list;
+	int			pos;
+	int			fd;
+	char		car;
 
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
