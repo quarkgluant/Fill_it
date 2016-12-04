0d600d2a41d6ed98d2451e84f71811d0856885c3
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2016-12-04 20:07:58 +0100

    nouvelle structure dans header

--- fill_it.h
+++ fill_it.h
@@ -26,16 +26,34 @@ typedef	struct		s_list
 {
 	char			data;
 	char			letter;
-	short			x;
-	short			y;
-	short			width;
-	short			height;
+	int				x;
+	int				y;
+	int				z;
+	int				width;
+	int				height;
 	struct s_list	*next;
 }					t_list;
 
+typedef struct 		tet_list
+{
+	char			tetra[16];
+	char			char_to_fill;
+	int				x;
+	int				y;
+	int				width;
+	int				height;
+	struct tet_list	*next;		
+}					t_tet_list;
+
 t_list				*ft_create_elem(char data);
+t_tet_list			*ft_create_tet(char *str);
 void				ft_list_push_back(t_list *begin_list, char data);
+void				tet_list_push_back(t_tet_list *begin_list, char *str);
 int					count_columns(t_list *list);
 t_list				*parse_file(char *path);
+void				ft_putchar(char c);
+void				ft_putstr(char const *str);
+void				ft_putnbr(int nb);
+size_t				ft_strlen(const char *str);
 
 #endif
